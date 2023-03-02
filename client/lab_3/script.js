78c3cde901be8befbf466c8d4ba259fafd5a5f4e
Pajuzie <99058304+Pajuzie@users.noreply.github.com>
2023-03-02 11:19:06 -0500

    updated lab 3

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -50,8 +50,8 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
-    if(slidePosition === totalSlides - 1){
-      slidePosition = 4;
+    if(slidePosition === 0){
+      slidePosition = totalSlides -1;
      } else {
       slidePosition -= 1;
      }
@@ -70,3 +70,8 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+document.querySelector('.prev') // Get the appropriate element (<button class="next">)
+  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
+    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
+    moveToPrevSlide(); // call the function above to handle this
+  });
