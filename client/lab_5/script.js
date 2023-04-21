842818badde4b4f3fe1230d75172325df071d0dc
Pajuzie <99058304+Pajuzie@users.noreply.github.com>
2023-04-21 01:39:59 -0400

    Lab5 updates

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -1,11 +1,23 @@
+function filterList(List, query){
+  return List.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase
+    const lowerCaseQuery = query.toLowerCase
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
+}
+
 /*
   Hook this script to index.html
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterbutton = document.querySelector('.filter_button')
+
+  let currentList = [];
+  
+  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
 
@@ -56,6 +68,19 @@ async function mainEvent() { // the async keyword means we can make API requests
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
   });
+
+  filterbutton.addEventListener('click', (event) =>{
+    console.log('clicked FilterButton');
+    
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+
+  })
 }
 
 /*
